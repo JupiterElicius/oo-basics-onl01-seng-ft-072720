b99e3e6526d57842235f013f0a1cc36d04b2c049
Nick Bazaldua <bazalnick@gmail.com>
2020-08-05 01:51:28 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -12,6 +12,30 @@ class Book
     @author = author
   end
 
+  def author
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def genre
+    @genre
+  end
+  
+  def turn_page
+  
+  end
+
 
 
 end
